0bccc176b80d9899df640b6b2f039374cc7ea50a
unknown <arielgomezmendez94@gmail.com>
2026-06-18 16:05:18 -0400

    Add aria-label attribute in the nav element

--- app/components/layout/Header.tsx
+++ app/components/layout/Header.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 const Header = () => {
   return (
     <header className="text-[#A1A1AA]">
-      <nav>
+      <nav aria-label="Navegación principal">
         <ul className="flex items-center justify-evenly mt-4">
           <li>
             <Link href="/">Inicio</Link>
