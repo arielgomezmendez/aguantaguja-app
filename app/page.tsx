55e8819c39dc4dc44cffb328c51bd5ecdbb0c278
unknown <arielgomezmendez94@gmail.com>
2026-06-28 21:09:13 -0400

    Add link in the Gallery section

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 import Image from "next/image";
 import Hero from "./components/home/Hero";
 import Artists from "./components/home/Artists";
-import Galery from "./components/home/Galery";
+import Gallery from "./components/home/Gallery";
 
 export default function Home() {
   return (
     <>
       <Hero />
       <Artists/>
-      <Galery/>
+      <Gallery/>
     </>
   );
 }
