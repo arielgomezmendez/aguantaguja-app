50fc86ba7c373d860e784e3220738e4472da0211
unknown <arielgomezmendez94@gmail.com>
2026-07-09 09:08:47 -0400

    Move the arrays specialities and artists from ArtistsPage to artistsData

---
+++ app/artists/artistsData.ts
@@ -0,0 +1,42 @@
+export const specialties = [
+  "Blackwork",
+  "Realismo",
+  "Fine line",
+  "Lettering",
+  "Diseño personalizado",
+];
+
+export const artists = [
+  {
+    id: "artist-01",
+    name: "Artista 01",
+    specialty: "Blackwork",
+    phone: "5354189474",
+    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
+    imagePosition: "8% 48%",
+  },
+  {
+    id: "artist-02",
+    name: "Artista 02",
+    specialty: "Fine line",
+    phone: "",
+    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
+    imagePosition: "31% 48%",
+  },
+  {
+    id: "artist-03",
+    name: "Artista 03",
+    specialty: "Realismo",
+    phone: "",
+    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
+    imagePosition: "50% 48%",
+  },
+  {
+    id: "artist-04",
+    name: "Artista 04",
+    specialty: "Diseño personalizado",
+    phone: "",
+    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
+    imagePosition: "69% 48%",
+  },
+];
--- app/artists/page.tsx
+++ app/artists/page.tsx
@@ -1,56 +1,13 @@
 import type { Metadata } from "next";
 import Link from "next/link";
 import ArtistCard from "../components/UI/ArtistCard";
+import { artists, specialties } from "./artistsData";
 
 export const metadata: Metadata = {
   title: "Artistas | Aguantaguja Tattoo Studio",
   description:
     "Conoce al equipo de artistas de Aguantaguja y los estilos que trabajamos en el estudio.",
 };
-
-const specialties = [
-  "Blackwork",
-  "Realismo",
-  "Fine line",
-  "Lettering",
-  "Diseño personalizado",
-];
-
-const artists = [
-  {
-    id: "artist-01",
-    name: "Artista 01",
-    specialty: "Blackwork",
-    phone: "5354189474",
-    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
-    imagePosition: "8% 48%",
-  },
-  {
-    id: "artist-02",
-    name: "Artista 02",
-    specialty: "Fine line",
-    phone: "",
-    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
-    imagePosition: "31% 48%",
-  },
-  {
-    id: "artist-03",
-    name: "Artista 03",
-    specialty: "Realismo",
-    phone: "",
-    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
-    imagePosition: "50% 48%",
-  },
-  {
-    id: "artist-04",
-    name: "Artista 04",
-    specialty: "Diseño personalizado",
-    phone: "",
-    instagram: "https://www.instagram.com/aguantaguja_tattoostudio/",
-    imagePosition: "69% 48%",
-  },
-  
-];
 
 export default function ArtistsPage() {
   return (
